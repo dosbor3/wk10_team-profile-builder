0663156e151ad36f162ed14026793443003528bb
I am Billie <dosbor3@gmail.com>
2022-02-03 15:02:57 -0600

    manager-object implements, tested, and complete

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,41 +1,37 @@
-const { TestWatcher } = require("jest");
-const Employee = require("../lib/Employee");
-const Manager = require("../lib/Manager");
+const Manager = require("../lib/Manager.js");
 
 
 jest.mock('../lib/Employee.js');
 
-test("create a manager object", () => {
-    const manager = new Manager("Jane Smith", "sj340010750", "jsemail50@gmail.com");
+test("create a manager object", ()  => {
+    const manager = new Manager();
 
-    expect(manager.name).toBe("Jane Smith");
-    expect(manager.id).toBe("sj340010750");
-    expect(manager.email).toBe("jsemail50@gmail.com");
-
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(String));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
 });
 
 test("gets manager's name", () => {
-    const manager = new Manager("John Smith", "1234567", "johnsmithemail@gmail.com");
-    expect(manager.getName()).toBe("John Smith");
+    const manager = new Manager();
+    expect(manager.getName()).toEqual(expect.any(String));
 });
 
 test("gets manager's id", () => {
-    const manager = new Manager("John Smith", "1234567", "johnsmithemail@gmail.com");
-    expect(manager.getId()).toBe("1234567");
+    const manager = new Manager();
+    expect(manager.getId()).toEqual(expect.any(String));
 });
 
 test("gets manager's email", () => {
-    const manager = new Manager("John Smith", "1234567", "johnsmithemail@gmail.com");
-    expect(manager.getEmail()).toBe("johnsmithemail@gmail.com");
+    const manager = new Manager();
+    expect(manager.getEmail()).toEqual(expect.any(String));
 });
 
-test("gets manager's role as an Employee object", (Employee) => {
-    const employee = new Employee(this.name, this.id, this.email);
+test("gets manager's role as object", () => {
+    const manager = new Manager();
 
-    expect(Employee.name).toMatchObject(this.name);
-    expect(Manager.id).toMatchObject(Employee.id);
-    expect(Manager.email).toMatchObject(Employee.email);    
+    expect(manager.name).toBe("John Smith");
+    expect(manager.id).toEqual(expect.any(String));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
 });
-
-
-
